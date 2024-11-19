5e7c93036b2cc3c05790c13fa2663f9ac23d39c1
chris-jt <chtronconi@gmail.com>
2024-11-19 10:44:32 -0300

    actions

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-//Codigo para definir un proveedor AWS y crear una instancia EC2 basica
+# Codigo para definir un proveedor AWS y crear una instancia EC2 basica
 provider "aws" {
   region = "us-east-1"
 }
@@ -6,22 +6,23 @@ provider "aws" {
 resource "aws_instance" "ejemplo" {
   ami           = "ami-012967cc5a8c9f891"
   instance_type = "t2.micro"
+
   tags = {
     Name = "instancia-ejemplo"
   }
 }
 
-//Ejecuta el siguiente comando para inicializar tu proyecto Terraform:
-// terraform init   -->Este comando descarga los plugins necesarios para el proveedor AWS.
+# Ejecuta el siguiente comando para inicializar tu proyecto Terraform:
+# terraform init   -->Este comando descarga los plugins necesarios para el proveedor AWS.
 
-// Para ver qué cambios realizará Terraform 
-// terraform plan
+# Para ver qué cambios realizará Terraform 
+# terraform plan
 
-// Para crear la infraestructura definida
-// terraform apply
+# Para crear la infraestructura definida
+# terraform apply
 
-// Verificar la creación de tu instancia EC2 en la consola de AWS o utilizando el siguiente comando de AWS CLI:
-// aws ec2 describe-instances --filters "Name=tag:Name,Values=instancia-ejemplo"
+# Verificar la creación de tu instancia EC2 en la consola de AWS o utilizando el siguiente comando de AWS CLI:
+# aws ec2 describe-instances --filters "Name=tag:Name,Values=instancia-ejemplo"
 
-// Destrucción de la infraestructura
-// terraform destroy
+# Destrucción de la infraestructura
+# terraform destroy
