ce2891d797a9090de8c97195893d5eb8fc2b3445
chris-jt <chtronconi@gmail.com>
2024-11-19 10:28:20 -0300

    se añaden ficheros para git actions

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 //Codigo para definir un proveedor AWS y crear una instancia EC2 basica
 provider "aws" {
-    region = "us_east-1"
+    region = "us-east-1"
 }
 
 resource "aws_instance" "ejemplo"{
-    ami = "ami-0c55b159cbfafe1f0"
+    ami = "ami-012967cc5a8c9f891"
     instance_type = "t2.micro"
     tags = {
         Name = "instancia-ejemplo"
